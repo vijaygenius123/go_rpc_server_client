53971e56790e6426dba590a7d00573889d5cd900
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-28 22:37:54 +0530

    Create A Simple AddItem To In Memory Database

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # go_rpc_server_client
+
+
+## Create A Basic CRUD
+
+Create a simple in memory db
+```go
+type Item struct{
+    title string
+    body string
+}
+
+var database []Item
+
+
+func AddItem(item Item) Item{
+    database = append(database, item)
+    return item
+}
+
+```
--- main.go
+++ main.go
@@ -5,8 +5,31 @@ import (
 )
 
 
+type Item struct {
+	title string
+	body string
+}
+
+var database []Item
+
+func AddItem(item Item) Item{
+	database = append(database, item)
+	return item
+}
+
 func main(){
 
 	fmt.Println("Hello")
+
+	fmt.Println("Initial Database State", database)
+	a := Item{"first","This Is The First Item"}
+	b := Item{"second","This Is The Second Item"}
+	c := Item{"third","This Is The Third Item"}
+
+	AddItem(a)
+	AddItem(b)
+	AddItem(c)
+
+	fmt.Println("Database State After Adding Items", database)
 
 }
